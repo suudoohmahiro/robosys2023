c48ef71731460ee04967fc014daf0458bb0c91e7
suudooh <suudooh@hogehoge.com>
2023-11-28 12:37:52 +0900

    Add ###STRANGE INPUT###

--- square-fifth_test.bash
+++ square-fifth_test.bash
@@ -17,4 +17,13 @@ out=$(echo 2 | ./square-fifth)
 16
 32" ] || ng ${LINENO}
 
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK
